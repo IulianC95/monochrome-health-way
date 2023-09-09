98dcd3d3af04409f8adc1c8b2aa6138948a42ce7
Iulian <iuliancuculici@gmail.com>
2023-09-09 15:38:08 +0300

    test

---
+++ src/app/sg/page.jsx
@@ -0,0 +1,32 @@
+export default function StyleGuide() {
+  return (
+    <>
+      <main className="style-guide">
+        <div className="box text-1">
+          <p>Khand font-family Text</p>
+        </div>
+        <div className="box text-2">
+          <p>Kolker Brush font-family Text</p>
+        </div>
+        <div className="box">
+          <div className="button button-style-1">
+            <button>New In</button>
+            
+          </div>
+        </div>
+        <br />
+        <div className="box">
+          <div className="button button-style-2">
+            <a href="/">view all</a>
+          </div>
+        </div>
+        <br />
+        <div className="box">
+          <div className="button button-style-3">
+            <button>submit</button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
