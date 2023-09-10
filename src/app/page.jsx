2a548323d35ca2586bc20938f1092acdc1989aa2
Iulian <iuliancuculici@gmail.com>
2023-09-10 21:01:51 +0300

    about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,29 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import MostOrdered from "@/components/main/most-ordered";
-import NewProductsTile from "@/components/main/new-products-tile";
-
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import About from '@/components/main/about';
+import MostOrdered from '@/components/main/most-ordered';
+import NewProductsTile from '@/components/main/new-products-tile';
 
 export default function Home() {
   return (
     <body>
       <div>
         <header className="my-5">
-        <Header></Header>
+          <Header></Header>
         </header>
         <section className="mb-1">
-        <NewProductsTile></NewProductsTile>
+          <NewProductsTile></NewProductsTile>
         </section>
-        <section>
+        <section className="mb-3">
           <MostOrdered></MostOrdered>
         </section>
-       <br />
+        <section>
+          <About></About>
+        </section>
+        <br />
 
         <Footer></Footer>
       </div>
     </body>
-  )
+  );
 }
---
+++ src/components/main/about.jsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <>
+      <section className="about container">
+        <div className="about-img">
+          <picture>
+            <img
+              src="/images/presentation.jpg"
+              alt=""
+              title=""
+              className="mobile-img img-fluid d-lg-none"
+            />
+            <img
+              src="/images/about.png"
+              alt=""
+              title=""
+              className="mobile-img d-none d-lg-block"
+            />
+          </picture>
+        </div>
+        <div className="about-text">
+          <h2>Traditional remedies</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p className="d-none d-lg-block">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
